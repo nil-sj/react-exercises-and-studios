c904577098feea23a7a4241e7a937080948dd140
Nil SJ <niladri123@gmail.com>
2024-03-10 19:05:30 -0400

    Completed Class 18 Exercises - React Part 1 - Part 1

--- part1/exercises/src/components/ChoresList.jsx
+++ part1/exercises/src/components/ChoresList.jsx
@@ -1,3 +1,16 @@
+import React from 'react';
+import classes from './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+
+   return (
+      <div>
+         <h3 className = {classes.choresHeading}>Chores I Want To Do Today</h3>
+         <ol>
+            <li className={classes.choresText}>Wash Clothes</li>
+            <li className={classes.choresText}>Wash Dishes</li>
+            <li className={classes.choresText}>Wash Car</li>
+         </ol>
+      </div>
+   );
 }
